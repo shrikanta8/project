1caf9600dc621629c8d8ac1709152ea0c9e85bfa
Shrikanta <kalantrishriji8@gmail.com>
2023-08-05 22:47:01 +0530

    payment model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -40,7 +40,11 @@ const userSchema = new Schema({
         default:'USER'
     },
     forgotPasswordToken: String,
-    forgotPasswordExpiry: Date
+    forgotPasswordExpiry: Date,
+    subscription: {
+        id: String,
+        status: String
+    }
 },{
     timestamps: true
 })
